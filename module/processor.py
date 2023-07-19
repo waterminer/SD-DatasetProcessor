bee5e050fdc8ffec60578db2f08de616a75465de
waterminer <420773173@qq.com>
2023-07-19 15:10:25 +0800

    整理

--- module/processor.py
+++ module/processor.py
@@ -5,7 +5,13 @@ from PIL import ImageEnhance
 import copy
 
 class Processor(Data):
-
+    #在这里定义处理方法
+    '''
+    编写规范如下:
+    def 处理名(data:Data,args):
+        #代码块
+        return data
+    '''
     def randomCrop(data,size):
         if not(data.size[0] <= size or data.size[1] <= size):
             x = random(1,data.size[0]-size)
@@ -20,11 +26,11 @@ class Processor(Data):
         return data
     
     def flip(data):
-        token=True
         data.img = data.img.transpose(Image.FLIP_LEFT_RIGHT)
         data.conduct+="_f"
         return data
 
+#一个自定义的异常
 class processorError(RuntimeError):
     pass
 
