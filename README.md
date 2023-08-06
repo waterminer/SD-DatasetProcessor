012aa7ae2cd4670e2a9081696d36deefb60a38ef
注水的矿工 <43202966+waterminer@users.noreply.github.com>
2023-08-07 03:05:45 +0800

    更新自动打标 (#9)
    * 加入子处理
    * 加入自动打标
    * 加入更多处理器
    * 修复BUG

--- README.md
+++ README.md
@@ -1,86 +1,33 @@
-# 说明文档
+# 一站式的图片数据集预处理工具包
 
-## 安装
+[说明文档](https://github.com/waterminer/SD-DatasetProcessor/blob/preview/doc/doc_cn.md)
 
-需要安装依赖
+✅批量处理图片  
+包括且不限于：
+* 批量翻转
+* 批量随机裁切
+* 图片对比度增强
 
-```bash
-pip install pil
-pip install numpy
-```
+✅批量处理标签  
+包括且不限于：
+* 批量删除标签
+* 批量插入标签
+* 批量修改标签
 
-## 使用方式
+✅过滤机制
 
-参考conf文件进行编写
+✅易于开发/维护
 
-```yaml
-path:
-  input: "" #输入路径
-  output: "" #输出路径
+✅自动打标（试验性）
 
-conduct:
-  - #处理组1 此处示意为将所有大于512的图片进行翻转
-    filters: #用于定义过滤器
-    - #过滤器1
-      filter: img_filter
-      arg: [512,-1]
-    processor: #用于定义处理器
-    - #处理器1
-      method: flip
-      arg: 512
+✅子处理（试验性）
 
-#以下为进阶示范
-  - #处理组2 此处示意为将1024~2048区间大小的图片进行缩放然后进行随机裁切
-    repeat: 3 #用于循环执行(可选)
-    filters: #用于定义过滤器
-    - #过滤器1
-      filter: img_filter
-      arg: [1024,2048]
-    processor: #用于定义处理器
-    - #处理器1
-      method: resize
-      arg: 0.5
-    - #处理器2
-      method: random_crop
-      arg: 512
-```
+## TODO
 
-## 处理器说明
+🚧重构自动打标代码
 
-### 图片处理
+🚧图片放大功能
 
-|名称|说明|参数|
-| -- | -- | -- |
-|random_crop|随机裁切矩形图片|图片分辨率（整数）|
-|flip|水平翻转图片| - |
-|resize|按比例重新调整大小|比例（浮点数）|
-|force_resize|将图片缩放至特定大小|数组，输入格式为[x,y]|
-|offset|将图片偏移n个像素|偏移量（整数）|
-|rotation|将图片选择n个角度|角度(整数)|
-|contrast_enhancement|对比度增强|-|
-|brightness_enhancement|亮度增强|-|
-|color_enhancement|饱和度增强|-|
-|random_enhancement|随机增强|-|
-|none|不做操作（用于特定场合）|-|
+🚧图形化界面
 
-### 标签处理
-
-|名称|说明|参数|
-| -- | -- | -- |
-|append_tag|在标签组末尾加上标签|标签（文本）|
-|remove_tag|删除标签组中符合条件的标签|标签（文本）|
-|insert_tag|在标签组开头插入标签|标签（文本）|
-|move_forward|将选定标签移到开头|标签（文本）|
-|rename_tag|重命名标签，将`A标签`重命名为`B标签`|['A标签','B标签']|
-
-## 过滤器说明
-
-负责过滤符合特定条件的数据
-
-|名称|说明|参数|
-| -- | -- | -- |
-|img_size|过虑特定尺寸的图片|数组，输入格式为[max,min]，缺省填-1 |
-|tag_filter|过滤掉特定标签|标签（文本）|
-|tag_selecter|须要包含特定标签|标签（文本）|
-|tag_is_not_none|只含有带标签的图片|-|
-|tag_is_none|只含有不带标签的图片|-|
+如果这个项目给您提供了帮助，不妨点一个⭐star，谢谢
--- main.py
+++ main.py
@@ -1,44 +1,38 @@
-from module import ProcessorError
-from module.uitl import (
-    data_list_builder,
-    filter_manager,
-    processor_manager,
-)
+from module import *
+
+from module.tools.tagger import Tagger
 
 import os
 import yaml
 
-with open("./conf.yaml", "r", encoding="utf-8") as f:
-    config = yaml.load(f.read(), yaml.FullLoader)
-# 设置
-input_dir = config.get('path').get('input')  # 输入目录
-output_dir = config.get('path').get('output')  # 输出目录
-
-conducts = config.get('conduct')
-
-def main(input_dir, output_dir, conducts):
-    data_list = data_list_builder(input_dir)
+def main(input_dir, output_dir, conducts,option:dict|None=None, tagger:Tagger|None=None):
+    data_list = data_list_builder(input_dir,tagger)
     if not (os.path.exists(output_dir)):
         os.mkdir(output_dir)
     i = 0
     for i in range(0, len(data_list)):
         data = data_list.pop()
-        for conduct in conducts:
-            data.id = i
-            filters = conduct.get('filters')
-            if filters:
-                if filter_manager(filters, data): continue
-            if bool(conduct.get('repeat')):
-                repeat = conduct.get('repeat')
-            else:
-                repeat = 1
-            for j in range(0, repeat):
-                data.repeat = j
-                try:
-                    processor_manager(conduct.get('processor'), data).save(output_dir)
-                except ProcessorError:
-                    break
+        data.id = i
+        data = conduct_manager(conducts,data,output_dir,option)
+        if data is None:
+            continue
+        else:
+            data.save(output_dir,option)
 
 
 if __name__ == "__main__":
-    main(input_dir,output_dir,conducts)
+    with open("./config.yaml", "r", encoding="utf-8") as f:
+        config = yaml.load(f.read(), yaml.FullLoader)
+    # 设置
+    input_dir = config.get('path').get('input')  # 输入目录
+    output_dir = config.get('path').get('output')  # 输出目录
+    # 参数
+    conducts = config.get('conduct')
+    option = config.get('option')
+    tagger = config.get('tagger')
+
+    if tagger:
+        if tagger['active']:
+           tagger=tagger_bulider(tagger)
+        else: tagger=None
+    main(input_dir,output_dir,conducts,option,tagger=tagger)
