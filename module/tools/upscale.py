98a13a8a6c00464128a2025b9de893b3282294f8
waterminer <420773173@qq.com>
2023-08-09 13:15:44 +0800

    update upscale

--- module/tools/upscale.py
+++ module/tools/upscale.py
@@ -1,59 +1,90 @@
 from huggingface_hub import hf_hub_download
-import numpy as np
-import cv2
-import realesrgan
+from realesrgan import RealESRGANer
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from torch import nn as nn
 
 from dataclasses import dataclass, field
 import os
 from enum import Enum,auto as enumauto
 
-from module import data
+from module import Data
 
 class ModelType(Enum):
-    R_ESRGEN_2X = enumauto()
-    R_ESRGEN_4X = enumauto()
-    R_ESRGEN_8X = enumauto()
-    CUSTOM=enumauto()
+    R_ESRGAN_2X = enumauto()
+    R_ESRGAN_4X = enumauto()
+    R_ESRGAN_8X = enumauto()
+    R_ESRGAN_ANIME6B_4X = enumauto()
+    CUSTOM = enumauto()
     
 @dataclass
 class UpcaleOption:
+    # downloadsorce:str = field(default="hugginface")
     force_download: bool = field(default=False)
-    model_type: ModelType = field(default=ModelType.R_ESRGEN)
-    model_path: str = field(default="./models")
+    model_type: ModelType = field(default=ModelType.R_ESRGAN_2X)
+    model_path: str = field(default="./models/real_esrgan")
     custom_model_name:str = field(default="")
+    custom_model:nn.Module = field(default=None)
+    custom_scale:int = field(default=2)
+    tile:int = field(default=0)
+    tile_pad:int = field(default=10)
+    pre_pad:int = field(default=10)
+    half:bool = field(default=False)
 
 class CustomModelError(RuntimeError): ...
 
-class CustomModelNotExsitError(CustomModelError): ...
-
-class UpscaleModel:
-    def __init__(self,option:UpcaleOption):
+class UpscaleModel(RealESRGANer):
+    def __init__(self,option:UpcaleOption|None=UpcaleOption()):
         match option.model_type.value:
-            case ModelType.R_ESRGEN_2X.value:
-                self.repo_id="ai-forever/Real-ESRGAN"
-                self.file="RealESRGAN_x2.pth"
-                return
-            case ModelType.R_ESRGEN_4X.value:
-                self.repo_id="ai-forever/Real-ESRGAN"
-                self.file="RealESRGAN_x4.pth"
-            case ModelType.R_ESRGEN_8X.value:
-                self.repo_id="ai-forever/Real-ESRGAN"
-                self.file="RealESRGAN_x8.pth"
+            case ModelType.R_ESRGAN_2X.value:
+                repo_id="ai-forever/Real-ESRGAN"
+                file="RealESRGAN_x2.pth"
+                model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+                scale=2
+            case ModelType.R_ESRGAN_4X.value:
+                repo_id="ai-forever/Real-ESRGAN"
+                file="RealESRGAN_x4.pth"
+                model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+                scale=4
+            case ModelType.R_ESRGAN_8X.value:
+                repo_id="ai-forever/Real-ESRGAN"
+                file="RealESRGAN_x8.pth"
+                scale=8
+                model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=8)
+            case ModelType.R_ESRGAN_ANIME_4X.value:
+                repo_id="ximso/RealESRGAN_x4plus_anime_6B"
+                file="RealESRGAN_x4plus_anime_6B.pth"
+                model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+                scale=4
             case ModelType.CUSTOM.value:
-                self.repo_id="custom"
+                repo_id="custom"
                 try:
-                    self.file=option.custom_model_name
-                except:
-                    print("UpcaleOption:custom_model_name is not exist!")
+                    file=option.custom_model_name
+                    model=option.custom_model
+                    scale=option.custom_scale
+                    if not os.path.exists(os.path.join(option.model_path,file)):
+                        raise ChildProcessError
+                except CustomModelError:
+                    print("UpcaleOption:custom_model is not exist!")
                     exit(1)
             case _:
                 raise RuntimeError
                 exit(1)
-        if option.model_type is not ModelType.CUSTOM.value and os.path.exists(os.path.join(option.model_path,self.file)):
+        print("Loading upscale model...")
+        if option.model_type is not ModelType.CUSTOM.value and (
+            not os.path.exists(os.path.join(option.model_path,file)) or option.force_download) :
             hf_hub_download(
-                self.repo_id,
-                self.file,
+                repo_id,
+                file,
+                cache_dir=option.model_path,
                 force_download=True,
-                force_filename=self.file
+                force_filename=file
             )
-        self.model = self.file
+        model_path = os.path.join(option.model_path,file)
+        dni_weight=None,
+        tile=option.tile,
+        tile_pad=option.tile_pad,
+        pre_pad=option.pre_pad,
+        half=option.half,
+        device=None,
+        gpu_id=None
+        super().__init__(scale, model_path, dni_weight, model, tile, tile_pad, pre_pad, half, device, gpu_id)
